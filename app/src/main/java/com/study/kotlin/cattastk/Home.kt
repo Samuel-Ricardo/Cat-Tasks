fc946fd2bf4beb860aaf60cc3adf7e340310db1e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-30 16:38:04 -0300

    Create: Home - Screen (view)

---
+++ app/src/main/java/com/study/kotlin/cattastk/Home.kt
@@ -0,0 +1,12 @@
+package com.study.kotlin.cattastk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
