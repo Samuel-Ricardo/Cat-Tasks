0f7a6cf2ec6bcb38792182097292e79076445650
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-31 18:34:36 -0300

    Update: View (layout)

---
+++ app/src/main/java/com/study/kotlin/cattastk/add_task.kt
@@ -0,0 +1,12 @@
+package com.study.kotlin.cattastk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_task : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
