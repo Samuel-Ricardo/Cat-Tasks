2eae80e6fa83e2c22c4ef3b7aaa187f44a54e34d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 00:13:19 -0300

    Update: Repository - TaskRepository (data.repositories)

--- app/src/main/java/com/study/kotlin/cattastk/data/repositories/TaskRepository.kt
+++ app/src/main/java/com/study/kotlin/cattastk/data/repositories/TaskRepository.kt
@@ -1,11 +1,12 @@
 package com.study.kotlin.cattastk.data.repositories
 
+import com.study.kotlin.cattastk.data.database.dao.TaskDAO
 import com.study.kotlin.cattastk.data.entity.TaskEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class TaskRepository(private val DAO: TaskRepository) {
+class TaskRepository(private val DAO: TaskDAO) {
 
     fun insert(task: TaskEntity) = runBlocking {
         launch (Dispatchers.IO){
