ae49abdf9952694244a18b579a3bf7fbce2b47ce
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-31 23:38:34 -0300

    Create: Entity - Task (data.entity)

---
+++ app/src/main/java/com/study/kotlin/cattastk/data/entity/Task.kt
@@ -0,0 +1,15 @@
+package com.study.kotlin.cattastk.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+import java.time.LocalTime
+
+@Entity
+data class Task (
+    @PrimaryKey(autoGenerate = true) val id:Int = 0,
+    val title:String,
+    val notes:String,
+    val date:LocalDate,
+    val time:LocalTime,
+)
