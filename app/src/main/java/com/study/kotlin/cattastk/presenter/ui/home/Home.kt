40d36f47834b3e98f66c26d4ab1a3e0932a49fdf
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 18:45:49 -0300

    Setup: Activity - Home (presenter.ui.home)

--- app/src/main/java/com/study/kotlin/cattastk/presenter/ui/home/Home.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/ui/home/Home.kt
@@ -3,11 +3,24 @@ package com.study.kotlin.cattastk.presenter.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.study.kotlin.cattastk.R
+import com.study.kotlin.cattastk.databinding.ActivityHomeBinding
+import com.study.kotlin.cattastk.presenter.adapter.task.TaskAdapter
 
 class Home : AppCompatActivity() {
 
+    val binding by lazy {ActivityHomeBinding.inflate(layoutInflater)}
+    val adapter by lazy {TaskAdapter()}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+
+        setup()
     }
+
+    private fun setup() {
+        TODO("Not yet implemented")
+    }
+
+
 }
