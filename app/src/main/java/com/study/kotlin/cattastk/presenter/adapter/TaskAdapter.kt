43629edb48d2d516b7108096ee39bf5d3660dcf4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 14:03:59 -0300

    Create: Class - DiffCallback (presenter.adapter)

---
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/TaskAdapter.kt
@@ -0,0 +1,18 @@
+package com.study.kotlin.cattastk.presenter.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.study.kotlin.cattastk.data.entity.Task
+
+class TaskAdapter {
+}
+
+class DiffCallback: DiffUtil.ItemCallback<Task>(){
+
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        TODO("Not yet implemented")
+    }
+}
