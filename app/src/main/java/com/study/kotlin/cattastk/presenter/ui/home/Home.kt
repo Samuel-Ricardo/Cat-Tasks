e466c01020709d42e0b0d2d98c9dc96dae48f474
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 19:12:41 -0300

    Create: Function - updateList (presenter.ui.home.Home)

--- app/src/main/java/com/study/kotlin/cattastk/presenter/ui/home/Home.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/ui/home/Home.kt
@@ -2,13 +2,27 @@ package com.study.kotlin.cattastk.presenter.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.study.kotlin.cattastk.App
 import com.study.kotlin.cattastk.R
 import com.study.kotlin.cattastk.databinding.ActivityHomeBinding
+import com.study.kotlin.cattastk.domain.TaskUseCase
 import com.study.kotlin.cattastk.presenter.adapter.task.TaskAdapter
+import com.study.kotlin.cattastk.presenter.viewmodel.MainViewModel
+import com.study.kotlin.cattastk.presenter.viewmodel.factory.MainViewModelFactory
 
 class Home : AppCompatActivity() {
 
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
+    }
+
     val binding by lazy {ActivityHomeBinding.inflate(layoutInflater)}
+    val viewModel by viewModels<MainViewModel> {
+        MainViewModelFactory(
+            TaskUseCase((application as App).repository)
+        )
+    }
     val adapter by lazy {TaskAdapter()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +42,10 @@ class Home : AppCompatActivity() {
         updateList();
     }
 
+    private fun updateList() {
+        viewModel.getAll().observe(this, {task ->
+            adapter.submitList(task)
+        })
+    }
 
 }
