4612c75dc04c8863c4d3b7854078b2416a38db98
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 16:01:32 -0300

    Setup: Activity - add_task (presenter.ui.main.task.create)

--- app/src/main/java/com/study/kotlin/cattastk/App.kt
+++ app/src/main/java/com/study/kotlin/cattastk/App.kt
@@ -3,9 +3,11 @@ package com.study.kotlin.cattastk
 import android.app.Application
 import com.study.kotlin.cattastk.data.database.AppDatabase
 import com.study.kotlin.cattastk.data.repositories.TaskRepository
+import com.study.kotlin.cattastk.domain.TaskUseCase
 
 class App: Application() {
 
     val database by lazy {AppDatabase.getDatabase(this)}
     val repository by lazy {TaskRepository(database.taskDAO())}
+    val taskUseCase by lazy {TaskUseCase(repository)}
 }
--- app/src/main/java/com/study/kotlin/cattastk/presenter/ui/task/create/Add_task.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/ui/task/create/Add_task.kt
@@ -2,12 +2,26 @@ package com.study.kotlin.cattastk.presenter.ui.task.create
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.study.kotlin.cattastk.App
 import com.study.kotlin.cattastk.R
+import com.study.kotlin.cattastk.databinding.ActivityAddTaskBinding
+import com.study.kotlin.cattastk.presenter.viewmodel.MainViewModel
+import com.study.kotlin.cattastk.presenter.viewmodel.factory.MainViewModelFactory
 
 class add_task : AppCompatActivity() {
 
+    val binding by lazy { ActivityAddTaskBinding.inflate(layoutInflater) }
+    val MainViewModel:MainViewModel by viewModels { MainViewModelFactory((application as App).taskUseCase) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        setContentView(binding.root)
+
+        setup()
+    }
+
+    private fun setup() {
+        TODO("Not yet implemented")
     }
 }
