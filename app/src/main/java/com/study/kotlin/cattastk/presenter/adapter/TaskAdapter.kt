4cc0581208c55893049c1453f4b567e614024ee8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 14:07:47 -0300

    Override: fun - areItemsTheSame (presenter.adapter.DiffCallBack)

--- app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/TaskAdapter.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/TaskAdapter.kt
@@ -8,9 +8,7 @@ class TaskAdapter {
 
 class DiffCallback: DiffUtil.ItemCallback<Task>(){
 
-    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun areItemsTheSame(oldItem: Task, newItem: Task) = oldItem == newItem
 
     override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
         TODO("Not yet implemented")
