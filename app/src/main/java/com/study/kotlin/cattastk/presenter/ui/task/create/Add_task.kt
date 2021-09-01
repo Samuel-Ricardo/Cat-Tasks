6e9b930cf1dbc0e9a0712daafb5ab3e344827a84
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 16:09:15 -0300

    Create: Function - setupPermissions (presenter.ui.task.create.add_task)

--- app/src/main/java/com/study/kotlin/cattastk/presenter/ui/task/create/Add_task.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/ui/task/create/Add_task.kt
@@ -1,13 +1,16 @@
 package com.study.kotlin.cattastk.presenter.ui.task.create
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import com.study.kotlin.cattastk.App
 import com.study.kotlin.cattastk.R
 import com.study.kotlin.cattastk.databinding.ActivityAddTaskBinding
 import com.study.kotlin.cattastk.presenter.viewmodel.MainViewModel
 import com.study.kotlin.cattastk.presenter.viewmodel.factory.MainViewModelFactory
+
 
 class add_task : AppCompatActivity() {
 
@@ -31,6 +34,18 @@ class add_task : AppCompatActivity() {
     }
 
     private fun setupPermissions() {
-        TODO("Not yet implemented")
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ),
+            1
+        )
     }
 }
