91dbfc4b920abd2bf8c33f6a79754c5fd18942f0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 14:43:07 -0300

    Create: function - getSummedUpText (util.text.Text)

--- app/src/main/java/com/study/kotlin/cattastk/util/text/Text.kt
+++ app/src/main/java/com/study/kotlin/cattastk/util/text/Text.kt
@@ -1,4 +1,14 @@
 package com.study.kotlin.cattastk.util.text
 
 class Text {
+
+    fun getSummedUpText(text: String):String {
+
+        if(text.length > 50){
+            return "${text.subSequence(0, 50)}..."
+        }else{
+            return text;
+        }
+
+    }
 }
