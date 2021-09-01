39f1f88ba34014090737cff42606e493db17fcc6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-01 14:15:51 -0300

    Create: ViewHolder - TaskViewHolder (presenter.adapter.task.viewholder)

---
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/task/viewholder/TaskViewHolder.kt
@@ -0,0 +1,14 @@
+package com.study.kotlin.cattastk.presenter.adapter.task.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.cattastk.data.entity.Task
+import com.study.kotlin.cattastk.databinding.TaskLayoutBinding
+
+class TaskViewHolder(
+    private val binding: TaskLayoutBinding
+): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Task){
+
+    }
+}
