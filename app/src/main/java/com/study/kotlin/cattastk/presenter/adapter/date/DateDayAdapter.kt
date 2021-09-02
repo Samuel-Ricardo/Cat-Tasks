174c46bbdc4e75be28730e18a680e3dc43fb3df2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-02 10:41:46 -0300

    Create: Adapter - DateDay (presenter.adapter.date)

--- app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/date/DateDayAdapter.kt
+++ app/src/main/java/com/study/kotlin/cattastk/presenter/adapter/date/DateDayAdapter.kt
@@ -1,5 +1,25 @@
 package com.study.kotlin.cattastk.presenter.adapter.date
 
-class DateDayAdapter {
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.ListAdapter
+import com.study.kotlin.cattastk.databinding.DateDayBinding
+import com.study.kotlin.cattastk.presenter.adapter.date.viewholder.DateDayViewHolder
+import com.study.kotlin.cattastk.presenter.adapter.task.viewholder.TaskViewHolder
+import java.time.LocalDate
 
+class DateDayAdapter: ListAdapter<LocalDate, DateDayViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DateDayViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DateDayBinding.inflate(inflater, parent, false)
+
+        return (DateDayViewHolder(binding))
+    }
+
+    override fun onBindViewHolder(holder: DateDayViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
 }
