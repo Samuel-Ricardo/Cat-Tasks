c729b1297fae4979212977bf89a48a05a5d3f0ea
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-02 22:41:20 -0300

    Create: Class - Time (domain.model)

--- app/src/main/java/com/study/kotlin/cattastk/domain/model/Date.kt
+++ app/src/main/java/com/study/kotlin/cattastk/domain/model/Date.kt
@@ -1,12 +1,5 @@
 package com.study.kotlin.cattastk.domain.model
 
-import android.system.Os
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.time.Instant
-import kotlin.properties.Delegates
-
-
 class Date{
 
     var year = 2021
---
+++ app/src/main/java/com/study/kotlin/cattastk/domain/model/Time.kt
@@ -0,0 +1,7 @@
+package com.study.kotlin.cattastk.domain.model
+
+class Time {
+
+    var hour = 0
+    var second = 0
+}
