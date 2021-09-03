51f30729862f0bd9195271c8375cf3166bc3383a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-02 22:43:04 -0300

    Create: Function - setData (domain.model.Time)

--- app/src/main/java/com/study/kotlin/cattastk/domain/model/Time.kt
+++ app/src/main/java/com/study/kotlin/cattastk/domain/model/Time.kt
@@ -3,5 +3,10 @@ package com.study.kotlin.cattastk.domain.model
 class Time {
 
     var hour = 0
-    var second = 0
+    var minutes = 0
+
+    private fun setData(hour: Int, minutes: Int){
+        this.hour = checkHour(hour)
+        this.minutes = checkMinutes(minutes)
+    }
 }
